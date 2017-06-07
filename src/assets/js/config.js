a6eb1c5470c4f52ef4f3be46f3d38bba9d37fd9a
linganmin <saboran@163.com>
2017-06-07 09:26:13 +0800

    modify https

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,5 +1,5 @@
 export default {
-    baseApi: 'http://www.ifilm.ltd/api/',
+    baseApi: 'https://www.ifilm.ltd/api/',
     // baseApi: 'http://192.168.188.63:7777/api/',
     // baseApi: 'http://192.168.1.106:7777/api',
 }
