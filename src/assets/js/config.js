17212012609b63450a1ff86d3007f39d8d2e15ae
lingan <minlingan@laoyuegou.com>
2018-09-12 17:34:37 +0800

    修改接口地址

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,5 +1,5 @@
 export default {
-    baseApi: 'https://www.ifilm.ltd/api/',
+    baseApi: 'https://ifilm.linganmin.cn/api/',
     // baseApi: 'http://192.168.188.63:7777/api/',
     // baseApi: 'http://192.168.1.106:7777/api',
 }
